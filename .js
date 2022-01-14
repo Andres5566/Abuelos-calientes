e26edba3a274d51fa6df8d9b7aaa68c91f31fc69
Andres5566 <92273641+Andres5566@users.noreply.github.com>
2022-01-14 14:55:14 -0500

    Create .js

---
+++ .js
@@ -0,0 +1,8 @@
+function Multiplicar() {
+    var numero1 = document.getElementById("numero1").value;
+    var numero2 = document.getElementById("numero2").value;
+
+    var Multiplicar = numero1 * numero2;
+
+    document.writeln(Multiplicar);
+}
